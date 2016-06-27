305b97e98f7cd161416eee5f2ede7081371c056f
tri3ans4h <apri.triansah@darmajaya.ac.id>
2016-06-27 11:09:42 +0700

    Update index.php

--- server/index.php
+++ server/index.php
@@ -1,4 +1,31 @@
 <?php
-
+//Mapping hardware
+$CONFIG_HARDWARE = 
+array(
+  array(
+    'device_id' => 1,
+    'tipe'  =>  'sensor',
+    'descp' => 'Sensor Ultrasonik',
+    'loc'   => 'Tandon Air',
+    'ip_address' =>'192.168.43.69',
+    'dependency'=>array(1)
+    ),
+  array(
+    'tipe'  =>  'relay',
+    'descp' => 'Relay Pompa',
+    'loc'   => 'Pompa',
+    'ip_address' =>'192.168.43.70',
+    'dependency'=>array(2)
+    ) ,
+    
+  array(
+    'tipe'  =>  'relay',
+    'descp' => 'Relay Lampu 1',
+    'loc'   => 'Ruang Tamu',
+    'ip_address' =>'192.168.43.71',
+    'dependency'=>array()
+    )  
+    
+  )
 
 ?>
